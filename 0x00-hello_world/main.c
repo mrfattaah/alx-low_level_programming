e7da1374266440dc2d942584aa620be9f4f2e75d
Abdul-Fattah <abdulfattahedos1000@gmail.com>
2021-09-01 21:49:48 -0400

    main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
-  return (0);
-}
+printf("Holberton School");
+return (0);
+
