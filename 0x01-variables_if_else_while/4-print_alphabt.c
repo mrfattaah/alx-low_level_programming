cfb621679c14b9c5a818b822c737f602b7c6ff16
Abdul-Fattah <abdulfattahedos1000@gmail.com>
2021-09-01 21:57:45 -0400

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Ommiting some letters
+ * printing alphabet without q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This project focuses on the variables, if, else and while in the C programming language
